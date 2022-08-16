041b75e80225c7b69e9c6481121202fdde7d3f12
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-08-16 23:10:56 +0300

    commit tree 4022d80e33c2426b6b0843a7f9fa59365ddb0618

--- methods.rb
+++ methods.rb
@@ -1,1 +1,42 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer()
+
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+
+
+def greet_with_default (name = "programmer") 
+    puts "Hello, #{name}!"
+end
+
+greet_with_default()
+
+
+def add(a,b) 
+    return a + b 
+end
+
+add(2, 5)
+
+def halve num
+    
+    if num.is_a?Integer
+        num / 2
+    else        
+        return nil
+    end
+end
+
+halve(6)
+halve("six")
